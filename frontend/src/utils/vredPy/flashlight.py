9b7a78344c87027bc065e860763739844a2f9a91
Green <changqing91@users.noreply.github.com>
2026-02-10 18:56:56 +0800

    1

--- frontend/src/utils/vredPy/flashlight.py
+++ frontend/src/utils/vredPy/flashlight.py
@@ -1,187 +1,145 @@
 import os
-global flashlight_state
-if 'flashlight_state' not in globals():
-    flashlight_state = {
-        'light': None,
-        'constraint': None,
-        'controller_constraint': None,
-        'controller_node': None,
-        'enabled': False
-    }
+from vrScenegraph import *
+from vrController import *
+from vrNodePtr import *
+from vrNodeUtils import *
+from vrFileIO import *
 
 class Flashlight:
     def __init__(self):
-        self.state = flashlight_state
-        self.right_controller = None
+        self.geo = None
+        self.trans = None
+        self.flashlight_handle = None
+        self.on = False
+        self.hand_node = None
+        self.active_controller = None
+        self.is_left_side = False
         try:
-            self.right_controller = vrDeviceService.getVRDevice("right-controller")
+            self.hand_node = vrDeviceService.getVRDevice("right-controller").getNode()
+            self.active_controller = vrDeviceService.getVRDevice("right-controller")
         except Exception:
-            self.right_controller = None
-        self.ensure_controller_model()
-        self.toggle()
-    def ensure_controller_model(self):
-        if self.state['controller_node']:
-            return self.state['controller_node']
-        controller_node = None
+            self.hand_node = None
+            self.active_controller = None
         try:
-            controller_node = findNode("VRControllerFlashlight")
+            vrHMDService.hmdStatusWillChange.connect(self.__on_hmd_status_will_change)
         except Exception:
-            controller_node = None
-        if controller_node and hasattr(controller_node, "isNull"):
+            pass
+        self.switch_on()
+
+    def switch_on(self):
+        if self.on:
+            return
+        if not self.hand_node:
+            return
+        self.get_geo().setActive(True)
+        self.on = True
+        self.constraint = vrConstraintService.createParentConstraint([self.hand_node], self.geo, False)
+        self.constraint.setVisualizationVisible(False)
+        if self.active_controller:
+            self.active_controller.signal().moved.connect(self.update_flashlight)
+            self.visualization_mode = self.active_controller.getVisualizationMode()
+            self.adjust_flashlight(self.active_controller)
+
+    def switch_off(self):
+        if not self.on:
+            return
+        try:
+            self.get_geo().setActive(False)
+        except Exception:
+            pass
+        self.on = False
+        try:
+            vrConstraintService.deleteConstraint(self.constraint)
+        except Exception:
+            pass
+        if self.active_controller:
             try:
-                if controller_node.isNull():
-                    controller_node = None
+                self.active_controller.signal().moved.disconnect(self.update_flashlight)
             except Exception:
                 pass
-        if not controller_node:
+
+    def get_geo(self):
+        if self.geo is not None and self.geo.isValid():
+            return self.geo
+        self.create_geo()
+        return self.geo
+
+    def create_geo(self):
+        for node in findNodes("VR_Flashlight"):
+            node.getParent().subChild(node)
+        root_node = getInternalRootNode()
+        self.geo = createNode("Transform3D", "VR_Flashlight", root_node, False)
+        self.trans = createNode("Transform3D", "FlashlightPos", self.geo, False)
+        self.load_model()
+        try:
+            self.flashlight_handle = vrNodeService.findNode("Housing_02", False, False, self.trans)
+        except Exception:
+            self.flashlight_handle = None
+        self.geo.setActive(False)
+        setIsVRNode(self.trans, True)
+        setIsVRNode(self.geo, True)
+
+    def load_model(self):
+        model_path = self.get_controller_osb_path()
+        if model_path:
+            nodes = loadOSB([model_path])
+        else:
+            nodes = []
+        for node in nodes:
+            self.trans.addChild(node)
+
+    def get_controller_osb_path(self):
+        base_dir = None
+        try:
+            base_dir = os.path.join(os.environ["USERPROFILE"], "Documents")
+        except Exception:
             try:
+                base_dir = os.path.join(os.environ["HOME"], "Documents")
+            except Exception:
                 base_dir = None
-                try:
-                    base_dir = os.path.join(os.environ['USERPROFILE'], 'Documents')
-                except Exception:
-                    try:
-                        base_dir = os.path.join(os.environ['HOME'], 'Documents')
-                    except Exception:
-                        base_dir = None
-                if base_dir:
-                    filepath = os.path.join(base_dir, 'Autodesk', 'Automotive', 'VRED')
-                    filename = os.path.join(filepath, 'VRControllerFlashlight.osb')
-                    if os.path.exists(filename):
-                        controller_node = loadGeometry(filename)
-                        try:
-                            controller_node.setName("VRControllerFlashlight")
-                        except Exception:
-                            pass
-            except Exception:
-                controller_node = None
-        self.state['controller_node'] = controller_node
-        return controller_node
-    def ensure_light(self):
-        if self.state['light']:
-            return self.state['light']
-        light = None
-        parent = None
-        controller_node = self.ensure_controller_model()
+        if not base_dir:
+            return None
+        filepath = os.path.join(base_dir, "Autodesk", "Automotive", "VRED")
+        filename = os.path.join(filepath, "VRControllerFlashlight.osb")
+        if os.path.exists(filename):
+            return filename
+        return None
+
+    def update_flashlight(self, device):
         try:
-            parent = vrLightService.getLightRoot()
-        except Exception:
-            try:
-                parent = vrScenegraphService.getRootNode()
-            except Exception:
-                parent = None
-        try:
-            light = vrLightService.createLight("Flashlight", "SpotLight", parent)
-        except Exception:
-            try:
-                light = createNode("SpotLight", "Flashlight")
-                if parent:
-                    addChilds(parent, [light])
-            except Exception:
-                light = None
-        if light and controller_node:
-            try:
-                addChilds(controller_node, [light])
-            except Exception:
-                pass
-        self.state['light'] = light
-        if self.state['light']:
-            try:
-                self.state['light'].setVisible(False)
-            except Exception:
-                pass
-            try:
-                self.state['light'].fields().setFloat("intensity", 8.0)
-            except Exception:
-                pass
-            try:
-                self.state['light'].fields().setFloat("coneAngle", 28.0)
-            except Exception:
-                pass
-            try:
-                self.state['light'].fields().setFloat("softAngle", 12.0)
-            except Exception:
-                pass
-        return self.state['light']
-    def disable(self):
-        try:
-            if self.state['constraint']:
-                vrConstraintService.deleteConstraint(self.state['constraint'])
+            if device.getVisualizationMode() != self.visualization_mode:
+                self.adjust_flashlight(device)
+                self.visualization_mode = device.getVisualizationMode()
         except Exception:
             pass
-        self.state['constraint'] = None
+
+    def adjust_flashlight(self, device):
         try:
-            if self.state['controller_constraint']:
-                vrConstraintService.deleteConstraint(self.state['controller_constraint'])
+            if device.getVisualizationMode() == 1:
+                self.set_hand_transform()
+                if self.flashlight_handle:
+                    self.flashlight_handle.setVisibilityFlag(True)
+            else:
+                self.set_controller_transform()
+                if self.flashlight_handle:
+                    self.flashlight_handle.setVisibilityFlag(False)
         except Exception:
             pass
-        self.state['controller_constraint'] = None
-        try:
-            self.state['light'].setVisible(False)
-        except Exception:
-            pass
-        try:
-            self.state['light'].setActive(False)
-        except Exception:
-            pass
-        try:
-            if self.state['controller_node']:
-                self.state['controller_node'].setActive(0)
-        except Exception:
-            pass
-        if self.right_controller:
-            try:
-                self.right_controller.setVisible(1)
-            except Exception:
-                pass
-        self.state['enabled'] = False
-    def enable(self):
-        controller_node = None
-        if self.right_controller:
-            try:
-                controller_node = self.right_controller.getNode()
-            except Exception:
-                controller_node = None
-        model_node = self.ensure_controller_model()
-        if controller_node:
-            try:
-                self.state['constraint'] = vrConstraintService.createParentConstraint([controller_node], self.state['light'], False)
-            except Exception:
-                self.state['constraint'] = None
-        if controller_node and model_node:
-            try:
-                self.right_controller.setVisible(0)
-            except Exception:
-                pass
-            try:
-                model_node.setActive(1)
-            except Exception:
-                pass
-            try:
-                controller_pos = getTransformNodeTranslation(controller_node, 1)
-                setTransformNodeTranslation(model_node, controller_pos.x(), controller_pos.y(), controller_pos.z(), True)
-            except Exception:
-                pass
-            try:
-                self.state['controller_constraint'] = vrConstraintService.createParentConstraint([controller_node], model_node, False)
-            except Exception:
-                self.state['controller_constraint'] = None
-        try:
-            self.state['light'].setVisible(True)
-        except Exception:
-            pass
-        try:
-            self.state['light'].setActive(True)
-        except Exception:
-            pass
-        self.state['enabled'] = True
-    def toggle(self):
-        print("Toggle Flashlight")
-        if not self.ensure_light():
-            print("Flashlight failed")
-            return
-        if self.state['enabled']:
-            self.disable()
-            return
-        self.enable()
+
+    def set_hand_transform(self):
+        if self.is_left_side:
+            setTransformNodeTranslation(self.trans, -17, -5, 60, False)
+            setTransformNodeRotation(self.trans, 180, 10, 0)
+        else:
+            setTransformNodeTranslation(self.trans, 17, -5, 60, False)
+            setTransformNodeRotation(self.trans, 180, -10, 0)
+
+    def set_controller_transform(self):
+        setTransformNodeTranslation(self.trans, 0, -50, 50, False)
+        setTransformNodeRotation(self.trans, 110, 0, 0)
+
+    def __on_hmd_status_will_change(self, active):
+        if not active:
+            self.switch_off()
 
 Flashlight()
